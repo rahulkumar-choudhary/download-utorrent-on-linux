333c9052553a30c90930733ad5987cf64d43918d
Rahulkumar Choudhary <59869006+Rahulkumar0909@users.noreply.github.com>
2022-11-03 15:18:06 +0530

    Add files via upload

---
+++ trun.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+
+# Extract uTorrent files to the /opt directory 
+sudo tar -xvzf ./softwares/utserver.tar.gz -C /opt/
+
+# Set permission on uTorrent-server folder
+sudo chmod -R 755 /opt/utorrent-server-alpha-v3_3/
+
+# Run the command to link uTorrent server to the /user/bin directory
+sudo ln -s /opt/utorrent-server-alpha-v3_3/utserver /usr/bin/utserver
+
+
+# wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
+sudo apt-get install ./softwares/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
+
+# Start uTorrent
+utserver -settingspath /opt/utorrent-server-alpha-v3_3/
+
+# open in browser: localhost:8080/gui
+# The username is admin and leave the password field empty.
